7b1b8e47cedfb2a3bec7b484b5accdca911f72c0
Jirattanun <49854272+Tien-Jirattanun@users.noreply.github.com>
2020-04-14 02:11:11 +0700

    Add files via upload

---
+++ input,output.py
@@ -0,0 +1,13 @@
+pi = 3.14;
+x = input();                                            #x keep input
+name = input("What is your name : ");                   #name keep input
+
+print(x);                                               #print : x
+print("hello %s" % name );                              #print : Hello %s=print string
+print("hello"+" "+name);                                #'+'can connect string
+print("pi is %f" %pi );                                 #%f = print float
+print("pi is ",pi)                                      #','can connect different variable type
+print("monday","tuesday","wednesday",sep = "," );       #in every space are use ","
+print("one",end=" ");                                   #end with " "
+print("Two",end=" ");
+print("tree",end=" ");
---
+++ variable.py
@@ -0,0 +1,19 @@
+x = 5;                  #int
+pi = 3.14;              #float
+name = "Tien";          #string
+room = ["1/1","1/2"]
+
+print(x);
+print(pi);
+print(name);
+
+x = 6;                  #change x to 6
+
+print(x);
+print(pi);
+print(name);
+
+a, b = 4, 3;            #when we want to make a variable we can use this way
+
+print(a);
+print(b);
