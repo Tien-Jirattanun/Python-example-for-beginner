bb7f0a6c56ec9a91a08db7a56b98377ce5a0d267
Jirattanun <49854272+Tien-Jirattanun@users.noreply.github.com>
2020-04-14 21:14:11 +0700

    Add files via upload

---
+++ function.py
@@ -0,0 +1,19 @@
+def hello() :               #baic function
+    print("hello");
+
+def pr(message) :           #basic function
+    print(message);
+
+def qM(x, y) :              #return function
+    return x + y;
+
+def create_button(id, color = '#ffffff', text = 'Button', size = 16):  #Keyword Arguments
+    print('Button ID: %d' % id);
+    print('Attributes:');
+    print('Color: %s' % color);
+    print('Text: %s' % text);
+    print('Size: %d px' % size);
+    print();
+
+z = lambda x,y : x+y;
+print(z(3,5));
