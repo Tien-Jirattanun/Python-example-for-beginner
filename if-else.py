877e83443a7efc5dbb6a81c95c67f7bf87d80a9a
Jirattanun <49854272+Tien-Jirattanun@users.noreply.github.com>
2020-04-14 16:41:51 +0700

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,10 @@
+score = input("your score : ");
+
+#if else
+
+if score == 100 :                       #first statement if score == 100 do it if not check another statement
+    print("full score");
+elif score > 50 :                       #second statement if score > 50 do it if not check another statement
+    print("pass");
+else :                                  #if not thing true do this statement
+    print("fail");
