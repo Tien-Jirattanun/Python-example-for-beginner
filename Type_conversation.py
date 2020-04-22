4b3f40efff262c8dd2240060fe2248c4182b4d75
Jirattanun <49854272+Tien-Jirattanun@users.noreply.github.com>
2020-04-22 23:48:36 +0700

    Add files via upload

---
+++ Type_conversation.py
@@ -0,0 +1,6 @@
+#structure of type coversation
+A = 5;
+
+A = chr(A);                         #not only car cando like this int or another type can do it
+print(type(A));                     #too just change chr to another type
+print('===================')
